03756e09e2072a43380fd565c5bf041dd59d681d
mrx0 <alex.vilkov@bk.ru>
2017-01-13 18:26:39 +0400

    ...

--- pricelist.php
+++ pricelist.php
@@ -128,7 +128,7 @@
 						$price = 0;
 						
 						//$query = "SELECT `price` FROM `spr_priceprices` WHERE `item`='".$items_j[$i]['id']."' ORDER BY `create_time` DESC LIMIT 1";
-						$query = "SELECT `price` FROM `date_from` WHERE `item`='".$items_j[$i]['id']."' ORDER BY `create_time` DESC LIMIT 1";
+						$query = "SELECT `price` FROM `spr_priceprices` WHERE `item`='".$items_j[$i]['id']."' ORDER BY `create_time` DESC LIMIT 1";
 											
 						$res = mysql_query($query) or die(mysql_error().' -> '.$query);
 
