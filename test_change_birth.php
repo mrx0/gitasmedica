ee04208da31398d43a36ba12cbb20986f3c877b5
mrx0 <alex.vilkov@bk.ru>
2017-08-22 19:01:00 +0300

    ...

--- test_change_birth.php
+++ test_change_birth.php
@@ -22,20 +22,28 @@
 		
 		foreach ($rez as $client){
 			
-			/*if (($client['birthday'] != -1577934000) && ($client['birthday'] != 0)){
+			if (($client['birthday'] != -1577934000) && ($client['birthday'] != 0)){
+			    
 				$newDate = date('Y-m-d', $client['birthday']);
+
+                $query = "UPDATE `spr_clients` SET `birthday2`='".$newDate."' WHERE `id`='".$client['id']."' AND `birthday2`='0000-00-00' LIMIT 1";
+
+                mysqli_query($msql_cnnct, $query) or die(mysqli_error($msql_cnnct).' -> '.$query);
+
 			}else{
 				$newDate = '0000-00-00';
 			}
 		
-			$query = "UPDATE `spr_clients` SET `birthday2`='".$newDate."' WHERE `id`='".$client['id']."' AND `birthday2`='0000-00-00' LIMIT 1";
+			/*$query = "UPDATE `spr_clients` SET `birthday2`='".$newDate."' WHERE `id`='".$client['id']."' AND `birthday2`='0000-00-00' LIMIT 1";
 			
 			mysqli_query($msql_cnnct, $query) or die(mysqli_error($msql_cnnct).' -> '.$query);*/
 			
-			if (date('Y-m-d', $client['birthday']) != $client['birthday2']){
+			/*if (date('Y-m-d', $client['birthday']) != $client['birthday2']){
 				echo $client['id'].' => '.$client['birthday'].' = '.date('Y-m-d', $client['birthday']).' => '.$client['birthday2'].'<br>';
-			}
-			
+			}*/
+
+			echo 'Ok';
+
 		}
 
 	}
