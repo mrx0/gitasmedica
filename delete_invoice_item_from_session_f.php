32fd53069f115a3d2615a88e44fc8e05ce2f4f3c
mrx0 <alex.vilkov@bk.ru>
2018-04-09 18:53:21 +0300

    исправил ошибку, когда не удалялось из наряда позиция

--- delete_invoice_item_from_session_f.php
+++ delete_invoice_item_from_session_f.php
@@ -11,22 +11,22 @@
 		//var_dump ($_POST);
 		
 		if ($_POST){
-			if (!isset($_POST['key']) || !isset($_POST['zub']) || !isset($_POST['client']) || !isset($_POST['zapis_id']) || !isset($_POST['filial']) || !isset($_POST['worker'])){
+			if (!isset($_POST['key']) || !isset($_POST['ind']) || !isset($_POST['client']) || !isset($_POST['zapis_id']) || !isset($_POST['filial']) || !isset($_POST['worker'])){
 				//echo json_encode(array('result' => 'error', 'data' => '<div class="query_neok">Что-то пошло не так</div>'));
 			}else{
-				//var_dump($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['zub']][$_POST['key']]);
+				//var_dump($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['ind']][$_POST['key']]);
 				if ($_POST['target'] === 'item'){
-					unset($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['zub']][$_POST['key']]);
+					unset($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['ind']][$_POST['key']]);
 				}elseif ($_POST['target'] === 'zub'){
-					unset($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['zub']]);
-					unset($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['mkb'][$_POST['zub']]);
+					unset($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['ind']]);
+					unset($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['mkb'][$_POST['ind']]);
 				}
 				
-				if ($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['t_number_active'] == $_POST['zub']){
-					if (empty($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['zub']])){
+				if ($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['t_number_active'] == $_POST['ind']){
+					if (empty($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data'][$_POST['ind']])){
 						$keys = array_keys($_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['data']);
 						//$firstKey = $keys[0];
-                        if ($_POST['zub'] != 0) {
+                        if ($_POST['ind'] != 0) {
                             $_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['t_number_active'] = $keys[0];
                         }else{
                             $_SESSION['invoice_data'][$_POST['client']][$_POST['zapis_id']]['t_number_active'] = 0;
