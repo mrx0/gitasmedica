867bfc592aae092388668fc9b1579a9a01ab344e
mrx0 <alex.vilkov@bk.ru>
2018-08-10 15:21:13 +0300

    ...

--- fl_my_tabels.php
+++ fl_my_tabels.php
@@ -15,7 +15,7 @@
 		//var_dump($_SESSION);
 		//var_dump(date("m"));
 
-        $filials_j = getAllFilials(true, false);
+        $filials_j = getAllFilials(true, true);
         //var_dump($filials_j);
 
         echo '
