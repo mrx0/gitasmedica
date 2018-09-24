79a1e004dd4a675bb36ef402411c67d32b144020
mrx0 <alex.vilkov@bk.ru>
2018-09-24 11:18:54 +0300

    исправление ошибки в ajax_show_result_stat_insure_f.php связанной с переменной $query и permissions.php

--- ajax_show_result_stat_insure_f.php
+++ ajax_show_result_stat_insure_f.php
@@ -10,6 +10,14 @@ if (empty($_SESSION['login']) || empty($_SESSION['id'])){
 }else{
     //var_dump ($_POST);
     if ($_POST){
+        include_once 'DBWork.php';
+        include_once 'functions.php';
+
+        //разбираемся с правами
+        $god_mode = FALSE;
+
+        require_once 'permissions.php';
+
         //$workerExist = false;
         $queryDopExist = false;
         $queryDopExExist = false;
@@ -26,13 +34,7 @@ if (empty($_SESSION['login']) || empty($_SESSION['id'])){
         $cosm_edit = false;
         $finance_edit = false;
 
-        include_once 'DBWork.php';
-        include_once 'functions.php';
 
-        //разбираемся с правами
-        $god_mode = FALSE;
-
-        require_once 'permissions.php';
 
         $msql_cnnct = ConnectToDB();
 
