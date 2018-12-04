cd5b2629602e2028c6e71531bb8fa959238d7e80
mrx0 <alex.vilkov@bk.ru>
2018-12-04 19:03:09 +0300

    ...

--- test_fl_createDailyReport.php
+++ test_fl_createDailyReport.php
@@ -20,6 +20,27 @@ if ($number != 0){
     }
 }
 
-var_dump($rez);
+//var_dump($rez);
+
+foreach ($rez as $item) {
+
+    var_dump($item['id']);
+
+    $nal = 0;
+    $beznal = 0;
+
+    $nal = $item['cashbox_nal'] + $item['cashbox_cert_nal'] + $item['temp_orto_nal'] + $item['temp_specialist_nal'] + $item['temp_analiz_nal'] + $item['temp_solar_nal'] - $item['temp_giveoutcash'];
+    $beznal = $item['cashbox_beznal'] + $item['cashbox_cert_beznal'] + $item['temp_orto_beznal'] + $item['temp_specialist_beznal'] + $item['temp_analiz_beznal'] + $item['temp_solar_beznal'];
+
+    var_dump($nal);
+    var_dump($beznal);
+
+    echo $nal . ' + ' . $beznal . ' = ' . $item['summ'];
+
+    var_dump($item['arenda']);
+
+    echo $nal . ' + ' . $beznal . ' + ' . $item['arenda'] . ' = ' . $item['itogSumm'];
+    
+}
 
 ?>
