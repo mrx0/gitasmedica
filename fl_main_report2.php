af7d2ad4951b4b66d988daf6d56c7c813f5ef00d
mrx0 <alex.vilkov@bk.ru>
2019-08-16 17:44:28 +0300

    ...

--- fl_main_report2.php
+++ fl_main_report2.php
@@ -1314,11 +1314,11 @@
             // - $bank_summ
             // - $director_summ
             // + $prev_month_filial_summ
-            // - $subtractions_summ_beznal
+            // + $subtractions_summ_beznal
             //);
             //var_dump($subtractions_summ_beznal);
 
-            $ostatok = $cashbox_nal + $arenda - $giveoutcash_summ - $subtractions_summ - $paidouts_temp_summ - $bank_summ - $director_summ + $prev_month_filial_summ - $subtractions_summ_beznal;
+            $ostatok = $cashbox_nal + $arenda - $giveoutcash_summ - $subtractions_summ - $paidouts_temp_summ - $bank_summ - $director_summ + $prev_month_filial_summ + $subtractions_summ_beznal;
 
             $ostatok = number_format($ostatok, 0, '.', ' ');
 
