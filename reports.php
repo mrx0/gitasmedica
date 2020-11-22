2eb03ab087a5f379b9e51cfe47d29ca5fb5c0fdd
mrx <alex.vilkov@bk.ru>
2020-11-22 21:11:10 +0300

    ...

--- reports.php
+++ reports.php
@@ -57,10 +57,15 @@
 //								<a href="stat_labor.php" class="b3">Заказы в лабораторию</a>
 //							</li>';
 
-			/*echo '				
+			/*echo '
 							<li class="cellsBlock" style="margin: 1px;">
 								<a href="stat_stomat5.php" class="b3">Все просроченные незакрытые напоминания стоматологов</a>
 							</li>';*/
+
+			echo '				
+							<li class="cellsBlock" style="margin: 1px;">
+								<a href="temp_notes_cert.php" class="b3">Выданные сертификаты</a>
+							</li>';
 			echo '
 						</ul>
 						<ul class="reportBlock" style="">
