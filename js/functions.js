5815e04b34a3397fd97316424b0ddf965aff91b7
mrx <alex.vilkov@bk.ru>
2021-04-05 01:23:21 +0800

    доделал отчет по лампам

--- js/functions.js
+++ js/functions.js
@@ -142,6 +142,50 @@
         return color;
     }
 
+    //Рандомный случайный цвет из списка
+    function randColor2() {
+        Colors = {};
+        Colors.names = {
+            aqua: "#00ffff",
+            blue: "#0000ff",
+            brown: "#a52a2a",
+            darkblue: "#00008b",
+            darkcyan: "#008b8b",
+            darkgrey: "#a9a9a9",
+            darkgreen: "#006400",
+            darkkhaki: "#bdb76b",
+            darkmagenta: "#8b008b",
+            darkolivegreen: "#556b2f",
+            darkorange: "#ff8c00",
+            darkorchid: "#9932cc",
+            darkred: "#8b0000",
+            darksalmon: "#e9967a",
+            darkviolet: "#9400d3",
+            fuchsia: "#ff00ff",
+            gold: "#ffd700",
+            green: "#008000",
+            indigo: "#4b0082",
+             lime: "#00ff00",
+            magenta: "#ff00ff",
+            maroon: "#800000",
+            navy: "#000080",
+            olive: "#808000",
+            orange: "#ffa500",
+            purple: "#800080",
+            violet: "#800080",
+            red: "#ff0000",
+            yellow: "#ffff00"
+        };
+
+        let color;
+        let count = 0;
+        for (let prop in Colors.names)
+            if (Math.random() < 1/++count)
+                color = prop;
+
+        return color;
+    }
+
 
     //Функция для создания бланка платежки с QR кодом в формате PDF
     function createPayBlankPdfQRCode(){
@@ -15649,7 +15693,7 @@
                 $('#res_table_tmpl').html("<div style='width: 120px; height: 32px; padding: 10px; text-align: center; vertical-align: middle; border: 1px dotted rgb(255, 179, 0); background-color: rgba(255, 236, 24, 0.5);'><img src='img/wait.gif' style='float:left;'><span style='float: right;  font-size: 90%;'> обработка...</span></div>");
             },
             success: function (res) {
-                // console.log(res);
+                 // console.log(res);
                 // console.log(res.query);
 
                 if (res.result == "success") {
@@ -15664,11 +15708,11 @@
                     let color, lamp_datas = [];
 
                     for (lamp_id in res.data_count) {
-                        var temp_lamp_datas = {};
+                        let temp_lamp_datas = {};
 
-                        color = randColor();
+                        color = randColor2();
 
-                        temp_lamp_datas['label'] = 'Какая-то лампа '+lamp_id;
+                        temp_lamp_datas['label'] = res.spr_lamps_j[lamp_id].descr;
                         temp_lamp_datas['backgroundColor'] = color;
                         temp_lamp_datas['borderColor'] = color;
                         temp_lamp_datas['fill'] = false;
